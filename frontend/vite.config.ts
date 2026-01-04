029781ad3abfa36e80543576870e13bdabe6a5fc
aryavp9 <aryav.pal@clipboardhealth.com>
2026-01-04 12:55:27 -0500

    resolving vite error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['galaxystockfrontend-production.up.railway.app'],
     port: 5173,
     proxy: {
       '/api': {
